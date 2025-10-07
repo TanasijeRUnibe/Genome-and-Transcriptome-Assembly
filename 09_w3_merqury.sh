62a0f4f56f0d4fd439d7c81c3d01835b31e3ff83
TanasijeRUnibe <tanasije.rakic@students.unibe.ch>
2025-10-07 22:02:14 +0200

    Add files via upload

---
+++ 09_w3_merqury.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=merqury
+#SBATCH --partition=pibu_el8
+#SBATCH --output=/data/users/trakic/assembly_annotation_course/output_err/output_merqury_%j.o
+#SBATCH --error=/data/users/trakic/assembly_annotation_course/output_err/error_merqury_%j.e
+
+WORKDIR=/data/users/trakic/assembly_annotation_course/
+READS=/data/users/trakic/assembly_annotation_course/RRS10/ERR11437326.fastq.gz
+OUTDIR=$WORKDIR/results/week3_merqury
+CONTAINER_PATH="/containers/apptainer/merqury_1.3.sif"
+
+mkdir -p $OUTDIR
+
+FLYE=$WORKDIR/for_busco/flye_assembly.fasta
+HIFIASM=$WORKDIR/for_busco/hifisam.asm.bp.p_ctg.fa
+LJA=$WORKDIR/for_busco/ija_assembly.fasta
+
+# Path inside container
+export MERQURY="/usr/local/share/merqury"
+
+# Building meryl DB from HiFi reads
+if [ ! -d "$OUTDIR/hifi.meryl" ]; then
+  echo "Building meryl DB from reads..."
+  apptainer exec --bind /data "$CONTAINER_PATH" \
+    meryl count k=21 output "$OUTDIR/hifi.meryl" $READS
+else
+  echo "Using existing meryl DB: $OUTDIR/hifi.meryl"
+fi
+
+# Running Merqury for each assembly
+for ASM in flye hifiasm lja; do
+    echo "Running Merqury for $ASM..."
+    mkdir -p "$OUTDIR/$ASM"
+    cd "$OUTDIR/$ASM"
+    
+    if [ "$ASM" == "flye" ]; then
+        ASMFILE="$FLYE"
+    elif [ "$ASM" == "hifiasm" ]; then
+        ASMFILE="$HIFIASM"
+    elif [ "$ASM" == "lja" ]; then
+        ASMFILE="$LJA"
+    fi
+
+    apptainer exec --bind /data "$CONTAINER_PATH" \
+      $MERQURY/merqury.sh "$OUTDIR/hifi.meryl" "$ASMFILE" "$ASM"
+done
